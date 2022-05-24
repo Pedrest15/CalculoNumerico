58ca53aabf32b5d97d869fac11b814187b166e0f
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2022-05-24 19:13:00 -0300

    Melhorando a identação

--- francis_off.m
+++ francis_off.m
@@ -7,19 +7,19 @@ function [V,D,k,erros] = francis_off(A,tol)
 %k: número de iterações que o algoritmo realizou
 %erros: vetor que guarda o erro em cada iteração
 
-n = size(A,1);
-V = eye(n);
-off = inf;
-k = 0;
-erros = []; %matriz de uma linha
-while off>tol
-[Q,R] = qr_decomp(A);
-A = R*Q;
-V = V*Q;
-off = sqrt(norm(A,'fro')^2 - sum(diag(A).^2));
-erros(end+1) = off;
-k += 1;
-end
-D = diag(A);
-D = sort(D, "descend");
+  n = size(A,1);
+  V = eye(n);
+  off = inf;
+  k = 0;
+  erros = []; %matriz de uma linha
+  while off>tol
+    [Q,R] = qr_decomp(A);
+    A = R*Q;
+    V = V*Q;
+    off = sqrt(norm(A,'fro')^2 - sum(diag(A).^2));
+    erros(end+1) = off;
+    k += 1;
+  end
+  D = diag(A);
+  D = sort(D, "descend");
 endfunction
