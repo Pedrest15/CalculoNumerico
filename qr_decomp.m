347a84b8757a341ca8b491207e6c7b4e6d3dccec
Pedro Lucas Andrade <78179865+Pedrest15@users.noreply.github.com>
2022-05-24 19:13:59 -0300

    Adicionando identação

--- qr_decomp.m
+++ qr_decomp.m
@@ -3,15 +3,16 @@ function [Q,R] = qr_decomp(A)
 %Q: matriz (m,n)
 %R: matriz triangular superior
 
-[m,n] = size(A);
-V = A;
-Q = zeros(m,n);
-R = zeros(n,n);
-for i=1:n
-R(i,i) = norm(V(:,i));
-Q(:,i) = V(:,i)/R(i,i);
-for j=i+1:n
-R(i,j) = Q(:,i)'*V(:,j);
-V(:,j) = V(:,j)- R(i,j)*Q(:,i);
-end
-end
+  [m,n] = size(A);
+  V = A;
+  Q = zeros(m,n);
+  R = zeros(n,n);
+  for i=1:n
+    R(i,i) = norm(V(:,i));
+    Q(:,i) = V(:,i)/R(i,i);
+    for j=i+1:n
+      R(i,j) = Q(:,i)'*V(:,j);
+      V(:,j) = V(:,j)- R(i,j)*Q(:,i);
+    end
+  end
+endfunction
